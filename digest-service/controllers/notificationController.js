5cf7d6fb477a837ca2a0fa5ba88a3a796512b686
FASNA FABIN.PP <faznafebin22@gmail.com>
2026-08-06 11:04:24 +0000

    Added GitHub Actions CI workflow

--- digest-service/controllers/notificationController.js
+++ digest-service/controllers/notificationController.js
@@ -1,5 +1,5 @@
 const notifications = require("../data/notifications");
-
+const axios = require("axios");
 // POST /notify
 const receiveNotification = (req, res) => {
 
@@ -18,7 +18,50 @@ const receiveNotification = (req, res) => {
     });
 
 };
+const getDigest = async (req, res) => {
+    try {
 
-module.exports = {
-    receiveNotification
+        const internsResponse = await axios.get("http://tracker-api:3000/interns");
+        const tasksResponse = await axios.get("http://tracker-api:3000/tasks");
+        const evaluationsResponse = await axios.get("http://tracker-api:3000/evaluations");
+
+        const interns = internsResponse.data;
+        const tasks = tasksResponse.data;
+        const evaluations = evaluationsResponse.data;
+
+        const completedTasks = tasks.filter(
+            task => task.status === "Done"
+        ).length;
+
+        let averageEvaluationScore = 0;
+
+        if (evaluations.length > 0) {
+
+            const totalScore = evaluations.reduce(
+                (sum, evaluation) => sum + evaluation.score,
+                0
+            );
+
+            averageEvaluationScore = totalScore / evaluations.length;
+        }
+
+        res.json({
+            totalInterns: interns.length,
+            totalTasks: tasks.length,
+            completedTasks,
+            averageEvaluationScore
+        });
+
+    } catch (error) {
+
+        console.log(error.message);
+
+        res.status(500).json({
+            message: "Unable to fetch data from Tracker API"
+        });
+
+    }
+};module.exports = {
+    receiveNotification,
+    getDigest
 };
--- digest-service/routes/notificationRoutes.js
+++ digest-service/routes/notificationRoutes.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    receiveNotification
+    receiveNotification,
+    getDigest
 } = require("../controllers/notificationController");
 
 router.post("/", receiveNotification);
+
+router.get("/digest", getDigest);
 
 module.exports = router;
