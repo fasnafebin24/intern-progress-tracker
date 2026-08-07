c0055e44576b994efad92c7207d1e7ba796d9611
FASNA FABIN.PP <faznafebin22@gmail.com>
2026-08-07 05:20:51 +0000

    Fix intern summary frontend integration

--- frontend/script.js
+++ frontend/script.js
@@ -1,31 +1,12 @@
-function loadSummary(){
-
-fetch("http://localhost:3000/interns/1/summary")
-
-.then(response=>response.json())
-
-.then(data=>{
-
-
-document.getElementById("totalTasks").innerText =
-data.totalTasks;
-
-
-document.getElementById("completedTasks").innerText =
-data.completedTasks;
-
-
-document.getElementById("averageScore").innerText =
-data.averageEvaluationScore;
-
-
-})
-
-.catch(error=>{
-
-console.log(error);
-
-});
-
-
+function loadSummary() {
+    fetch("https://zany-carnival-5gjqgqx5wwp4h4gp4-3000.app.github.dev/interns/1/summary")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("totalTasks").innerText = data.totalTasks;
+            document.getElementById("completedTasks").innerText = data.completedTasks;
+            document.getElementById("averageScore").innerText = data.averageEvaluationScore;
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
