bbf001e5901e5f648bd9cbf7c6712e50674f6c0f
FASNA FABIN.PP <faznafebin22@gmail.com>
2026-09-15 11:30:32 +0000

    Fix JWT secret for automated tests

--- tracker-api/tests/setup.js
+++ tracker-api/tests/setup.js
@@ -1,3 +1,4 @@
+process.env.JWT_SECRET = process.env.JWT_SECRET || "test_jwt_secret";
 const mongoose = require("mongoose");
 const { MongoMemoryServer } = require("mongodb-memory-server");
 const bcrypt = require("bcryptjs");
